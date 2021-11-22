0151aa12c5cc93f138a2a3e64a04356c0f5c63c2
unknown <Kauaa780@gmail.com>
2021-11-21 23:37:29 -0300

    first-commit

---
+++ Desktop/Calculadora_IMC/JS/calc.js
@@ -0,0 +1,13 @@
+let nome = document.getElementsById(nome);
+
+let altura = document.getElementsById(altura);
+
+let peso = document.getElementsById(peso);
+
+let calculo = document.getElementsById(calculo)
+
+let resultado = document.getElementsById(resultado);
+
+alert(nome);
+
+console.log("Teste");

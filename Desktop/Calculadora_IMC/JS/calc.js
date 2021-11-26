64db2c7c3bc6243e20740d1a3aeba488065ccd5e
Kauã <Kauaa780@gmail.com>
2021-11-26 07:10:52 -0300

    css atualização

--- Desktop/Calculadora_IMC/JS/calc.js
+++ Desktop/Calculadora_IMC/JS/calc.js
@@ -8,14 +8,34 @@ function imc(){
     let resultado = document.getElementById('resultado');
 
     if(nome !== '' && altura !== '' && peso !== ''){
-        alert('Valor Preenchido');
+        
+        let valorIMC = Math.round(peso / (altura * altura));
+
+        resultado.textContent = (`Seu IMC é ${valorIMC}`);
+
+        /*
+        if (valorIMC < 17){
+
+            resultado.textContent = valorIMC("Seu IMC está abaixo dos recomendados");         
+   
+        }
+        if (valorIMC > 17, valorIMC < 34){
+
+            resultado.textContent = valorIMC("Seu IMC está entre o recomendado");
+
+        }
+        if(valorIMC > 34){
+
+            resultado.textContent = valorIMC("Seu IMC está muito alto!");
+
+        }
+        */
+
     }else{
-        resultado.textContent = ("Preencha todos os campos");
+       // resultado.textContent = "Preencha os campos.";
     }
 
 }
 
 
-calculo.addEventListener('click', imc);
-
-
+calculo.addEventListener( 'click', imc);
